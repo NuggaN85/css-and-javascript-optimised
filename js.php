e98ae393840ad93b3deb4d5a61957c7a769e0362
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-05 21:23:12 +0100

    Update js.php

--- js.php
+++ js.php
@@ -1,25 +1,28 @@
 <?php 
 $jsFiles = glob('*.js');
 foreach($jsFiles as $jsFile){ 
-$basename = basename($jsFile);
-$gzname   = $basename.'.gz';
-file_put_contents($gzname, gzencode( file_get_contents($basename),9));
+  $basename = basename($jsFile);
+  $gzname   = $basename.'.gz';
+  file_put_contents($gzname, gzencode(file_get_contents($basename),9));
 } 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+  <style>
+    h1 {
+      font-family: arial;
+      font-size: 30px;
+      font-weight: bold;
+      color: #006400;
+    }
+  </style>
+</head>
 <body>
-
-<style>
-h1 {font-family:arial;font-size:30px;font-weight:bold;color:#006400}
-</style>
-
-<center>
-<br /><br /><br /><br /><br />
-<h1>Done in javascript!</h1>
-</center>
-
+  <center>
+    <br /><br /><br /><br /><br />
+    <h1>Done in javascript!</h1>
+  </center>
 </body>
 </html>
