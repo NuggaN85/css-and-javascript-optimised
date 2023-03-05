3a86d73dfa5ca7f643e9e86b7d4f1a400a14e7ad
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-05 21:22:21 +0100

    Update css.php

--- css.php
+++ css.php
@@ -1,25 +1,28 @@
 <?php 
 $cssFiles = glob('*.css');
 foreach($cssFiles as $cssFile){ 
-$basename = basename($cssFile);
-$gzname   = $basename.'.gz';
-file_put_contents($gzname, gzencode( file_get_contents($basename),9));
+  $basename = basename($cssFile);
+  $gzname   = $basename.'.gz';
+  file_put_contents($gzname, gzencode(file_get_contents($basename),9));
 } 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+  <style>
+    h1 {
+      font-family: arial;
+      font-size: 30px;
+      font-weight: bold;
+      color: #FFA500;
+    }
+  </style>
+</head>
 <body>
-
-<style>
-h1 {font-family:arial;font-size:30px;font-weight:bold;color:#FFA500}
-</style>
-
-<center>
-<br /><br /><br /><br /><br />
-<h1>Done in stylesheet!</h1>
-</center>
-
+  <center>
+    <br /><br /><br /><br /><br />
+    <h1>Terminé.</h1>
+  </center>
 </body>
 </html>
