ade7d35dcb812b6f06451f9cd6cf31004052b490
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-08-19 19:32:06 +0200

    Update speed-up.php

--- speed-up.php
+++ speed-up.php
@@ -1,27 +1,24 @@
+<!DOCTYPE html>
 <html>
-  <head>
-    <script>
-      function loadSites() {
-        setTimeout(() => {
-          seite2();
-        }, 1);
-      }
-      
-      function seite2() {
-        parent.info.location.href = "css/css.php";
-        setTimeout(() => {
-          seite3();
-        }, 4500);
-      }
-      
-      function seite3() {
-        parent.info.location.href = "js/js.php";
-      }
-      
-      window.onload = loadSites;
-    </script>
-  </head>
-  <body>
-    <div id="info"></div>
-  </body>
+<head>
+  <script>
+    function loadSites() {
+      setTimeout(seite2, 1);
+    }
+    
+    function seite2() {
+      parent.info.location.href = "css/css.php";
+      setTimeout(seite3, 4500);
+    }
+    
+    function seite3() {
+      parent.info.location.href = "js/js.php";
+    }
+    
+    window.onload = loadSites;
+  </script>
+</head>
+<body>
+  <div id="info"></div>
+</body>
 </html>
