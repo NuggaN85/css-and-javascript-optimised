4dd316842bdf6ad5342fa48b685f967e0525ef74
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-08-19 19:30:19 +0200

    Update css.php

--- css.php
+++ css.php
@@ -1,9 +1,22 @@
 <?php 
 $cssFiles = glob('*.css');
-foreach($cssFiles as $cssFile){ 
-  $basename = basename($cssFile);
-  $gzname   = $basename.'.gz';
-  file_put_contents($gzname, gzencode(file_get_contents($basename),9));
+foreach ($cssFiles as $cssFile) {
+  if (is_file($cssFile)) {  // Vérifier si le chemin est bien un fichier
+    $basename = basename($cssFile);
+    $gzname = $basename . '.gz';
+    $fileContent = file_get_contents($basename);
+
+    if ($fileContent !== false) {  // Vérifier si la lecture du fichier a réussi
+      $compressedContent = gzencode($fileContent, 9);
+      if ($compressedContent !== false) {  // Vérifier si la compression a réussi
+        file_put_contents($gzname, $compressedContent);
+      } else {
+        echo "Échec de la compression du fichier $basename.";
+      }
+    } else {
+      echo "Échec de la lecture du fichier $basename.";
+    }
+  }
 } 
 ?>
 
@@ -12,7 +25,7 @@ foreach($cssFiles as $cssFile){
 <head>
   <style>
     h1 {
-      font-family: arial;
+      font-family: Arial, sans-serif;
       font-size: 30px;
       font-weight: bold;
       color: #FFA500;
@@ -20,9 +33,8 @@ foreach($cssFiles as $cssFile){
   </style>
 </head>
 <body>
-  <center>
-    <br /><br /><br /><br /><br />
+  <div style="text-align: center; padding-top: 100px;">
     <h1>Terminé.</h1>
-  </center>
+  </div>
 </body>
 </html>
